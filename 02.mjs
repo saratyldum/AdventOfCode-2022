c9b7a9fecc1d64b6a099262c5bdd6e1dd9bccef2
saratyldum <98975412+saratyldum@users.noreply.github.com>
2022-12-16 20:43:04 +0100

    made part1  solution into a function

--- 02.mjs
+++ 02.mjs
@@ -6,54 +6,62 @@ const lines = readFileSync("day02.txt", { encoding: "utf-8" }) // read day??.txt
   .split("\n") // Split on newline
   .map((line) => line.split(' '));
 
+function getInput() {
+  return [...lines];
+} 
 
-const rounds = [];
+function part1() {
+  const lines = getInput();
 
+  const rounds = [];
 
-for (let i = 0; i < 1; i++) {
-  lines.map((line) => {
-   const  opponent = line[0];
-   const you = line[1];
-    rounds.push({ opponent, you });
-  })
-};
+  for (let i = 0; i < 1; i++) {
+    lines.map((line) => {
+     const  opponent = line[0];
+     const you = line[1];
+      rounds.push({ opponent, you });
+    })
+  };
+  
+   let score = 0;
+   
+   for (let round of rounds) {
+    let opponent = round.opponent;
+    let you = round.you;
+   
+    let points = 0;
+   
+    if (opponent === "A") {
+      if (you === "Y") {
+       points = 8;
+      } else if (you === "X") {
+       points = 4;
+      } else if (you === "Z"){
+       points = 3;
+      }
+    } else if (opponent === "B") {
+      if (you === "Y") {
+       points = 5;
+      } else if (you === "X") {
+       points = 1;
+      } else if (you === "Z"){
+       points = 9;
+      }
+    } else if (opponent === "C"){ 
+      if (you === "Y") {
+       points = 2;
+      } else if (you === "X") {
+       points = 7;
+      } else if (you === "Z"){
+       points = 6;
+      }
+    }
+   
+    score += points;
+   }
+   
+   console.log(score); 
+}
 
- 
- let score = 0;
- 
- for (let round of rounds) {
-	let opponent = round.opponent;
-	let you = round.you;
- 
-	let points = 0;
- 
-	if (opponent === "A") {
-	  if (you === "Y") {
-		 points = 8;
-	  } else if (you === "X") {
-		 points = 4;
-	  } else if (you === "Z"){
-		 points = 3;
-	  }
-	} else if (opponent === "B") {
-	  if (you === "Y") {
-		 points = 5;
-	  } else if (you === "X") {
-		 points = 1;
-	  } else if (you === "Z"){
-		 points = 9;
-	  }
-	} else if (opponent === "C"){ 
-	  if (you === "Y") {
-		 points = 2;
-	  } else if (you === "X") {
-		 points = 7;
-	  } else if (you === "Z"){
-		 points = 6;
-	  }
-	}
- 
-	score += points;
- }
- 
- console.log(score); 
+part1();
+
