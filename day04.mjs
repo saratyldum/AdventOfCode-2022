f55713fc707275d75e5f266c13fd7aeaacd860fe
saratyldum <98975412+saratyldum@users.noreply.github.com>
2022-12-19 15:39:23 +0100

    day04

--- day04.mjs
+++ day04.mjs
@@ -1,11 +1,12 @@
+import { on } from "node:events";
 import { readFileSync } from "node:fs";
 
 const lines = readFileSync("day04.txt", { encoding: "utf-8" }) // read day??.txt content
   .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
   .trim() // Remove starting/ending whitespace
-  .split("\n"); // Split on newline
+  .split("\n") // Split on newline
+	// .map((line) => line.split(','));
 
-	console.log(lines);
 
 // Return a new object to avoid side effects between part 1 and 2
 function getInput() {
@@ -13,14 +14,46 @@ function getInput() {
 }
 
 function part1() {
-  const input = getInput();
+  const lines = getInput();
 	
-  //do something here
+	const result = lines.map((line) => {
+		const [interval1, interval2] = line
+		.split(',')
+		.map((interval) => interval.split('-').map(Number))
+		.sort((a, b) => {
+			const oneSize = a[1] - a[0];
+			const twoSize = b[1] - b[0];
+			return twoSize - oneSize;
+		});
+
+		const checkContaining = interval1[0] <= interval2[0] && interval1[1] >= interval2[1];
+
+		console.log({ interval1, interval2, checkContaining})
+
+		return checkContaining ? 1 : 0;
+	});
+	console.log(result.reduce((a, b) => a + b, 0));
+
 }
 
 function part2() {
-  const input = getInput();
-  //do something here
+  const lines = getInput();
+	
+	const result = lines.map((line) => {
+		const [first, second] = line
+		.split(',')
+		.map((interval) => interval.split('-').map(Number))
+		.sort((a, b) => {
+			const oneSize = a[1] - a[0];
+			const twoSize = b[1] - b[0];
+			return twoSize - oneSize;
+		});
+
+		const overlap = first[1] >= second[0] && second[1] >= first[0] ;
+
+		return overlap ? 1 : 0;
+	});
+	console.log(result.reduce((a, b) => a + b, 0));
 }
 
 part1();
